4bded2e0d5b0af84a80287bd11d53571b1490124
Saloni Modi <salonimodi@Salonis-MBP.lan>
2023-11-01 19:24:03 -0700

    initial commit

---
+++ src/AppRouter.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import App from './App'; 
+// import Home from './pages/Home'; 
+
+// function AppRouter() {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route path="/page/home" exact component={Home} /> {/* Define the Home page as the root route */}
+//         <Route path="/app" component={App} /> {/* Your existing App component */}
+//         {/* Add more routes for other pages here */}
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+// export default AppRouter;
---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      {/* Add content for your home page here */}
+    </div>
+  );
+}
+
+export default Home;
